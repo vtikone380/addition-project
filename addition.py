5da41a8255e3f20c944a82cfe20f35264085b7ac
vtikone380e <vtikone380@gmaile.com>
2024-05-28 00:44:16 +0530

    Update: Add three numbers

--- addition.py
+++ addition.py
@@ -3,6 +3,12 @@
 def add_two_numbers(a, b):
     return a + b
 
+def add_three_numbers(a, b, c):
+    return a + b + c
+
 if __name__ == "__main__":
-    result = add_two_numbers(5, 3)
-    print(f"Sum of 5 and 3 is {result}")
+    result_two = add_two_numbers(5, 3)
+    print(f"Sum of 5 and 3 is {result_two}")
+
+    result_three = add_three_numbers(5, 3, 2)
+    print(f"Sum of 5, 3, and 2 is {result_three}")
